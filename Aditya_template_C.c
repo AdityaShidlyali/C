16d138a7c232fc1b2671d5f633b2df7e7df6a410
AdityaShidlyali <adityamshidlyali@gmail.com>
2020-07-27 09:48:30 +0530

    C template added

---
+++ Aditya_template_C.c
@@ -0,0 +1,43 @@
+//Author : Aditya Shidlyali
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define SI(x) scanf("%d", &x)
+#define SLI(x) scanf("%ld", &x)
+#define SLLI(x) scanf("%lld", &x)
+#define SULLI(x) scanf("%ulld", &x);
+#define SS(s) scanf("%s", s)
+#define PI(x) printf("%d\n", x)
+#define PLI(x) printf("%ld\n", x)
+#define PLLI(x) printf("%lld\n", x)
+#define PULLI(x) printf("%ulld\n", x)
+#define PS(s) printf("%s\n", s)
+#define REP(i, a, b) for (int i = a; i <= b; ++i)
+#define REPR(i, a, b) for (int i = b; i >= a; --i)
+#define MOD 1000000007
+#define INF INFINITY
+#define FLUSH       \
+    fflush(stdout); \
+    fflush(stdin);
+#define MPI 3.1415926535897932384626
+
+typedef long int li;
+typedef long long int lli;
+typedef unsigned long long int ulli;
+
+/*---------------main function---------------*/
+
+int main() {
+    int t = 1;
+    SI(t);
+
+    while (t--) {
+
+        FLUSH;
+    }
+
+    return 0;
+}
